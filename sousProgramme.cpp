6fed5785028c45983ceadc5f35c9d9980a68b543
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2024-10-15 14:22:18 +0200

    Création du projet

---
+++ sousProgramme.cpp
@@ -0,0 +1,5 @@
+//
+// Created by emmanuel.a on 15/10/2024.
+//
+
+#include "sousProgramme.h"
---
+++ sousProgramme.h
@@ -0,0 +1,8 @@
+//
+// Created by emmanuel.a on 15/10/2024.
+//
+
+#ifndef SOUSPROGRAMME_H
+#define SOUSPROGRAMME_H
+
+#endif //SOUSPROGRAMME_H
